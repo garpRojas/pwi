a1cb48aedebad1b1ab360264fc962fe494210b5e
garpRojas <grojaspineda@gmail.com>
2015-09-21 20:58:13 -0600

    practica 2

---
+++ P#2 navegacion/app.js
@@ -0,0 +1,15 @@
+angular.module('navigation', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+
+	.when('/', {
+	    templateUrl: 'views/home.html'
+	})
+	.when('/products', {
+	    templateUrl: 'views/products.html'
+	})
+	.otherwise({
+	  	redirectTo: '/'
+	});
+}]);
